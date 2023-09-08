9f599892f709ee39c53f7c7e4bc9faf3ab5458d4
chulanpro5 <chulanpro5@gmail.com>
2023-09-08 23:58:01 +0700

    add: store model, store search

--- stores/models.py
+++ stores/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 from users.models import CustomUser as User
 from django.utils import timezone
+from django.contrib.postgres.search import TrigramSimilarity
+from django.db.models import Q
 
 # Create your models here.
     
+class StoreManager(models.Manager):
+    def similar_to(self, query, threshold):
+        return self.annotate(
+            name_similarity=TrigramSimilarity('name', query),
+            address_similarity=TrigramSimilarity('address', query),
+        ).filter(
+            Q(name_similarity__gt=threshold) | Q(address_similarity__gt=threshold)
+        ).order_by('-name_similarity', '-address_similarity')
+
+# class StoreManager(models.Manager):
+#     def similar_to(self, query, threshold):
+#         return self.annotate(
+#             similarity = TrigramSimilarity('name', query),
+#         ).filter(similarity__gt=threshold).order_by('-similarity')
+
 class Store(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=255)
@@ -12,6 +29,8 @@ class Store(models.Model):
     longitude = models.FloatField(blank=False)
     avg_rating = models.FloatField(default=0)
     image_link = models.CharField(max_length=255)
+
+    objects = StoreManager()
 
     def __str__(self):
         return self.name
--- stores/views.py
+++ stores/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render
 from .models import Store
 from django.db.models import Q
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 @csrf_exempt
 def search(request):
-    query = request.GET.get('query', '')  # Get the search query from the request
-    
-    # Perform a search on stores by name and address
-    results = Store.objects.filter(
-        Q(name__icontains=query) | Q(address__icontains=query)
-    )
+    if request.method == 'GET':
+        query = request.GET.get('query', '')  # Get the search query from the request
+        limit = int(request.GET.get('limit', 10))  # Get the number of results to display from the request
+        
+        # # Perform a search on stores by name and address
+        # results = Store.objects.filter(
+        #     Q(name__icontains=query) | Q(address__icontains=query)
+        # )
 
-    # Serialize the results to JSON
-    serialized_results = [
-        {'name': store.name, 'address': store.address, 'latitude': store.latitude, 'longitude': store.longitude}
-        for store in results
-    ]
+        # fuzzy search
+        threshold = 0.2
+        results = Store.objects.similar_to(query, threshold)[:limit]
 
-    return JsonResponse({'results': serialized_results})
+        # Serialize the results to JSON
+        serialized_results = [
+            {'name': store.name, 'address': store.address, 'latitude': store.latitude, 'longitude': store.longitude}
+            for store in results
+        ]
+
+        return JsonResponse({'results': serialized_results})
+    else:
+        return HttpResponse("Search")
 
 # def test_connection(request):
 #     # Retrieve the first 10 stores from the database
