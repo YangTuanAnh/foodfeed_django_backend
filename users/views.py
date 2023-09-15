4c9b9bdbde4c2c0b0415c71682daa6789878a5d0
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-15 20:20:25 +0800

    debug

--- users/views.py
+++ users/views.py
@@ -151,7 +151,8 @@ def friends(request):
             
         friends_json = serializers.serialize('json', user_friends)
 
-        #The following is for debugging reason
+        #The following lines are for debugging reason
+        print("????????", friends_json)
         print(json.load(friends_json))
         
         return JsonResponse(json.load(friends_json), status=200)
@@ -181,7 +182,11 @@ def make_friend(request, user_id):
         
 def suggestions(request):
     if request.method=="GET":
+        print("1")
         suggest_users = User.objects.exclude(request.user)
+        print("2")
         suggest_users = User.objects.order_by('?')[:5]
+        print("3")
         users_json = serializers.serialize('json', suggest_users)
+        print(users_json)
         return JsonResponse(json.load(users_json), status=200)
